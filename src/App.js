825dbf32ef91a8f9a27195740ceb0cd49897c297
waterdrag0n <sychang0422@gmail.com>
2021-05-03 22:23:34 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer'
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const customer = [
+  {
+    'id':1,
+    'image':'https://placeimg.com/64/64/any',
+    'name':'홍길동',
+    'birthday':'010101',
+    'gender':'M',
+    'job':'학생'
+  },
+  {
+    'id':2,
+    'image':'https://placeimg.com/64/64/any',
+    'name':'ㄱㄴㄷ',
+    'birthday':'020202',
+    'gender':'M',
+    'job':'학생'
+  },
+  {
+    'id':3,
+    'image':'https://placeimg.com/64/64/any',
+    'name':'ㅁㅂㅅ',
+    'birthday':'030303',
+    'gender':'F',
+    'job':'학생'
+  }
+]
+
+class App extends React.Component {
+  render(){
+    return (
+      <div>
+        {
+          customer.map(c=>{
+            return (
+              <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/>
+            );
+          })
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
